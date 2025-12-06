b307aebae977cc51fd9439701bb4f2dfd1e1ea53
daily1mostafa <daily1mostafa@gmail.com>
2025-12-06 22:31:00 +0200

    implementation of some Backtracking Algorithm functions

--- backtrackingAlgorithm.py
+++ backtrackingAlgorithm.py
@@ -1,17 +1,13 @@
 def sortItems(itemsList):
+    return sorted(itemsList, reverse=True)
 
-    pass
+def placeItem(item, binIndex, usedBins, binRemainingCapacities):
+    usedBins[binIndex].append(item)
+    binRemainingCapacities[binIndex] -= item
 
-
-def placeItem(item, bin, binCapacity=None):
-
-    pass
-
-
-def removeItem(item, bin, binCapacity=None):
-
-    pass
-
+def removeItem(item, binIndex, usedBins, binRemainingCapacities):
+    usedBins[binIndex].remove(item)
+    binRemainingCapacities[binIndex] += item
 
 def calculateLowerBound(remainingItems, binCapacity):
 
@@ -32,15 +28,42 @@ def initializeSolution(items, binCapacity):
 
     pass
 
+def findFeasibleBins(item, binRemainingCapacities):
+    indices = []
+    for i, remaining in enumerate(binRemainingCapacities):
+        if item <= remaining:
+            indices.append(i)
+    return indices
 
-def findFirstFitBin(item, currentBins, binCapacity):
+def backtrack(currentIndex, usedBins, binRemainingCapacities, binCapacity, bestSolution, sortedItemsList):
+    # Base case: all items have been placed
+    if currentIndex == len(sortedItemsList):
+        if len(usedBins) < len(bestSolution):
+            bestSolution.clear()
+            bestSolution.extend(copyBins(usedBins))
+        return
 
-    pass
+    item = sortedItemsList[currentIndex]
 
+    # Remaining items (including current item) for lower bound pruning
+    remainingItems = sortedItemsList[currentIndex:]
+    if pruneBranch(usedBins, bestSolution, remainingItems, binCapacity):
+        return
 
-def backtrack(currentIndex, usedBins, binCapacity, bestSolution, sortedItemsList):
+    # Try placing in existing bins (all feasible choices)
+    feasibleBins = findFeasibleBins(item, binRemainingCapacities)
+    for i in feasibleBins:
+        placeItem(item, i, usedBins, binRemainingCapacities)
+        backtrack(currentIndex + 1, usedBins, binRemainingCapacities,
+                  binCapacity, bestSolution, sortedItemsList)
+        removeItem(item, i, usedBins, binRemainingCapacities)
 
-    pass
+    # Try placing in a new bin
+    usedBins.append([item])
+    binRemainingCapacities.append(binCapacity - item)
+    backtrack(currentIndex + 1, usedBins, binRemainingCapacities, binCapacity, bestSolution, sortedItemsList)
+    usedBins.pop()
+    binRemainingCapacities.pop()
 
 
 def solveBinPacking(items, binCapacity):
