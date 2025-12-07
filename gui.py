056a6238291b33fa1ae0a25e1434adef2f78df53
Ninazu00 <ba.mohammad2005@gmail.com>
2025-12-07 21:52:13 +0200

    Added drawBinFill

--- gui.py
+++ gui.py
@@ -5,4 +5,16 @@ class GUI:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("1D Bin Packing Solver")
-        pass
+        self.root.geometry("1920x1080")
+        self.root.mainloop()
+    def drawBinFill(self,fillRate, binNumber):
+        barlength = 20
+        filled = int(fillRate*barlength)
+        empty = barlength - filled
+        bar = f"Bin {binNumber} : "
+        bar += "|" + ("█"*filled) + ("-"*empty) + "| " + str(fillRate*100) + "%"
+        return bar
+
+test = GUI()
+
+print(test.drawBinFill(0.81,1))
